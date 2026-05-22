1e939c9fbb442634828c3ccb95a95f50e2c657f5
MohitPoudel <iammohitpdl@gmail.com>
2026-05-22 17:52:22 +0545

    arrow function

---
+++ 02_arrowFN.js
@@ -0,0 +1,30 @@
+ //FUNCTIONS
+
+// function product (a,b){
+//     return a*b
+//  }
+//  let result = product(5,4) 
+//  console.log("The Product of a and b is : " + result)
+
+
+ //Arrow Functions
+
+product = (a,b) => {
+    return a*b
+}
+let results=product(5,4)
+console.log("The product of a and b is " + results)
+
+
+sum = (c,d) => c+d
+let res=sum(10,15)
+console.log("The sum of c and d is " + res)
+
+div = e => e / 10
+let result1=div(50)
+console.log("The division is " + result1)
+
+Nopara = () => 10*10
+let result2=Nopara()
+console.log(result2)
+
